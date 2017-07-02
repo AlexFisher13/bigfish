20303d51fd9c04c6d017828dc3c3c093c498de0a
Tkachenko AI <alexfisher13@yandex.ru>
2017-07-02 09:57:55 +0300

    Comments commit

--- src/main/java/com/fish/controller/FirstController.java
+++ src/main/java/com/fish/controller/FirstController.java
@@ -7,36 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@Controller //Аннотация указывает что это бин, и его нужно подгружать при старте приложения. HTTP запросы обрабатываются контроллером.
 public class FirstController {
-    /*Попадаем сюда на старте приложения*/
-    /* Аннотация @Controller служит для сообщения Spring'у о том, что данный класс является bean'ом
-    и его необходимо подгрузить при старте приложения.
-    Аннотацией @RequestMapping(value = "/") сообщаем, что данный контроллер будет обрабатывать запрос, URI которого "/" */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView main() {
-        ModelAndView modelAndView = new ModelAndView();
-        /*Специальный класс ModelAndView, который агрегирует все параметры модели и имя отображения
-        * подробнее можно почитать тут http://www.seostella.com/ru/article/2012/04/23/spring-3-i-controller-chast-1.html*/
+    //Аннотацией @RequestMapping(value = "/") сообщаем, что данный контроллер будет обрабатывать запрос, URI которого "/" */
+    @RequestMapping(value = "/", method = RequestMethod.GET) //RequestMethod - метод доступа. GET - запрашивает ресурсы. POST - отправляет данные на заданный ресурс.
+    public ModelAndView main() {    //Попадаем сюда на старте приложения
+        ModelAndView modelAndView = new ModelAndView(); //ModelAndView - cпециальный класс, который агрегирует все параметры модели и имя отображения
+        // подробнее можно почитать тут http://www.seostella.com/ru/article/2012/04/23/spring-3-i-controller-chast-1.html
+        //Теперь нам необходимо записать в модель атрибут, а так же объект, который будет соответствовать этому атрибуту.
         modelAndView.addObject("userJSP", new User()); //создаем нового юзера, и даем имя этому бину userJSP
-        modelAndView.setViewName("index"); //направляемся на страницу index
-        return modelAndView;
+        //Здесь используется имя userJSP, который ссылается на модель POJO класса User.class.
+        //Здесь специально разделил названия атрибута и модели, т.к. во многих примерах всегда используется model=user, object=user, name=user и т.п.. А потом нельзя понять куда что пишется и на что ссылается..*/
+        modelAndView.setViewName("index"); //Тут мы устанавливаем имя представления - index.
+        return modelAndView; //После чего мы отправляемся в dispatcher-servlet к org.springframework.web.servlet.view.InternalResourceViewResolver
     }
 
-    /*как только на index.jsp подтвердится форма
-    <spring:form method="post"  modelAttribute="userJSP" action="check-user">,
-    то попадем вот сюда
-     */
-    @RequestMapping(value = "/check-user")
+    //Как только на index.jsp подтвердится форма <spring:form method="post"  modelAttribute="userJSP" action="check-user">, то попадем вот сюда
+    @RequestMapping(value = "/check-user") //Контролле обрабатывает запрос /check-user и потом нас будет отправлять на страницу "secondPage"
     public ModelAndView checkUser(@ModelAttribute("userJSP") User user) {
-        ModelAndView modelAndView = new ModelAndView();
-
-        //имя представления, куда нужно будет перейти
-        modelAndView.setViewName("secondPage");
-
-        //записываем в атрибут userJSP (используется на странице *.jsp объект user
-        modelAndView.addObject("userJSP", user);
-
+        ModelAndView modelAndView = new ModelAndView(); //создаем новую модель
+        modelAndView.setViewName("secondPage");//устанавливаем имя представления куда будем переходить
+        modelAndView.addObject("userJSP", user); //записываем в атрибут модель с который мы работаем - userJSP (используется на странице *.jsp объект user)
         return modelAndView; //после уйдем на представление, указанное чуть выше, если оно будет найдено.
     }
 
--- src/web/WEB-INF/views/index.jsp
+++ src/web/WEB-INF/views/index.jsp
@@ -2,8 +2,8 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
-<%--Создаем форму с использованием тегов от spring (смотрите начало страницы), указываем метод формы (post или get),
-атрибут модели (используется в контроллере из описания выше) и что произойдет при подтверждении (сабмите) формы
+<%--Создаем форму с использованием тегов от spring, указываем метод формы (post или get),
+атрибут модели (нашу модельс которой работаем) и что произойдет при подтверждении (сабмите) формы
 (после нажатия на кнопку в нашем случае).--%>
 
 <html>
@@ -13,15 +13,15 @@
 </head>
 
 <body>
-<spring:form method="post"  modelAttribute="userJSP" action="check-user">
+<spring:form method="post"  modelAttribute="userJSP" action="check-user"> <%--Спринг-форма, post - будет отправлять данные, в модель userJSP, и послее подтверждения формы отправит нас по запросу check-user--%>
 
-    Name: <spring:input path="name"/> (path="" - указывает путь, используемый в modelAttribute=''. в нашем случае User.name)  <br/>
+    Name: <spring:input path="name"/>   <br/>  <%--(path="" - указывает путь, используемый в modelAttribute=''. в нашем случае User.name)--%>
     Password: <spring:input path="password"/>   <br/>
     <spring:button>Next Page</spring:button>
 
 </spring:form>
 
-<form action=""></form>
+<!--<form action=""></form>-->
 
 </body>
 
